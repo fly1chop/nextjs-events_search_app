7c82b2f16220fdb2920ae05ba1fd644c6f555429
Jane Ro <janero95@gmail.com>
2023-01-31 00:50:57 +0900

    feat: add icon svg files

---
+++ components/icons/index.js
@@ -0,0 +1,5 @@
+import DateIcon from './date-icon.js';
+import AddressIcon from './address-icon.js';
+import ArrowRightIcon from './arrow-right-icon.js';
+
+export { DateIcon, AddressIcon, ArrowRightIcon };
